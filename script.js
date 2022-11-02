0f05c78b58bf6e27838b0a28390dc94543d27641
Yehoyakim88 <yehoyakim@mail.de>
2022-11-02 10:36:08 +0100

    added horizontal and vertical winner lines

--- script.js
+++ script.js
@@ -77,24 +77,32 @@ function setField(id) {
 
 function checkForWin() {
     let winner;
+    let classToAdd;
+    let line_transform;
 
     if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        classToAdd = 'horLineFirstRow';
     }
     if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        classToAdd = 'horLineSecondRow';
     }
     if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        classToAdd = 'horLineLastRow';
     }
     if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        classToAdd = 'vertLineFirstCol';
     }
     if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        classToAdd = 'vertLineSecondCol';
     }
     if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
+        classToAdd = 'vertLineLastCol';
     }
     if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
@@ -107,5 +115,13 @@ function checkForWin() {
         alert(`${winner} hat gewonnen!`);
         document.getElementById('end-screen').classList.remove('d-none');
         document.getElementById('end-screen').classList.add('endScreen');
+        drawLineForWin(classToAdd);
     }
+}
+
+function drawLineForWin(classToAdd) {
+    let content = document.getElementById('id-content');
+
+    document.getElementById('win-line').classList.add(classToAdd)
+    document.getElementById('win-line').classList.remove('d-none');
 }
